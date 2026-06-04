8a833fb9d699af541768fac911adac2f1e4c5e68
tsubame556 <yamamonkeikou@gmail.com>
2026-06-04 15:03:09 +0900

    Update API URL with Mac local IP address

--- ScannerApp_Swift/FaceCaptureViewController.swift
+++ ScannerApp_Swift/FaceCaptureViewController.swift
@@ -292,7 +292,7 @@ class FaceCaptureViewController: UIViewController, ARSessionDelegate, ARSCNViewD
         }
         
         // 2. サーバーへ送信 (Multipart Form)
-        let url = URL(string: "http://192.168.x.x:8000/api/v1/generate_avatar")! // 実際のGPUサーバーのIPに変更
+        let url = URL(string: "http://10.46.3.210:8000/api/v1/generate_avatar")! // 実際のGPUサーバーのIPに変更
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         
